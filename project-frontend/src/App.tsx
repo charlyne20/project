3db26570ed2f35f7cfa3456145ab743ac0fa2f42
paglobal <jackamoah.pa@gmail.com>
2023-07-27 16:00:53 +0000

    simplified frontend and created backend base

--- project-frontend/src/App.tsx
+++ project-frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import DecorativeSide from "./DecorativeSide";
-import LoginForm from "./LoginForm";
-
 function App() {
   return (
-    <div className="flex w-screen h-screen overflow-hidden" id="app">
-      <DecorativeSide />
-      <LoginForm />
+    <div className="flex flex-col justify-center items-center w-screen h-screen overflow-hidden">
+      <p>Breast cancer diagnosis</p>
+      <p>Upload Breast Ultrasound images</p>
+      <p>Only Jpg, jpeg and png files allowed!</p>
+      <input
+        type="file"
+        id="image"
+        name="image"
+        accept="image/png, image/jpeg"
+      />
+      <p>or</p>
+      <p>Drag & Drop</p>
+      <p>Ultrasound images here</p>
     </div>
   );
 }
